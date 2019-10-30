f7d2aa43384e1ef56df91d7f45afdf5a4f836e01
Mohamed Ahmed <mahmoud_mx14@yahoo.com>
2019-10-30 09:48:02 +0200

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,16 @@
-
 $('body').scrollspy({ target: '.navmenu' });
 
 // When we click on the LI
-$("ul.qcontrols li").click(function(){
-  // If this isn't already active
-  if (!$(this).hasClass("active")) {
-    // Remove the class from anything that is active
-    $("ul.qcontrols li.active").removeClass("active");
-    // And make this active
-    $(this).addClass("active");
-  }
+$("ul.qcontrols li").click(function() {
+    // If this isn't already active
+    if (!$(this).hasClass("active")) {
+        // Remove the class from anything that is active
+        $("ul.qcontrols li.active").removeClass("active");
+        // And make this active
+        $(this).addClass("active");
+    }
 });
-
+$('[data-toggle="tooltip"]').tooltip();
 
 //WOW Scroll Spy
 var wow = new WOW({
@@ -20,71 +19,71 @@ var wow = new WOW({
 });
 wow.init();
 
-jQuery(document).ready(function( $ ) {
-        $('.circle').circleProgress({
+jQuery(document).ready(function($) {
+    $('.circle').circleProgress({
         value: 0.70,
         startAngle: 4.75,
         size: 200,
         fill: { gradient: ["#FFFFFF"] }
-      });
-        $('.two').circleProgress({
-          value: 0.75,
-        });
-        $('.three').circleProgress({
-          value: 0.50,
-        });
-        $('.four').circleProgress({
-          value: 0.90,
-        });
+    });
+    $('.two').circleProgress({
+        value: 0.75,
+    });
+    $('.three').circleProgress({
+        value: 0.50,
+    });
+    $('.four').circleProgress({
+        value: 0.90,
+    });
 
-   //Clients carousel Slider
+    //Clients carousel Slider
     $('#clients-carousel').owlCarousel({
         navigation: false, // Show next and prev buttons
         slideSpeed: 400,
         paginationSpeed: 800,
         autoPlay: true,
-        pagination : false,
-        items : 4,
-        itemsCustom : false,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [980,3],
-        itemsTablet: [768,2],
+        pagination: false,
+        items: 4,
+        itemsCustom: false,
+        itemsDesktop: [1199, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
         itemsTabletSmall: false,
-        itemsMobile : [479,1],
+        itemsMobile: [479, 1],
     });
-       $('#menu').click(function(){
-       $('#menu').not(this).removeClass("active");
-       $(this).toggleClass("active");
-      });
+    $('#menu').click(function() {
+        $('#menu').not(this).removeClass("active");
+        $(this).toggleClass("active");
+    });
 
-  });
+});
 
-  
+
 //ScrollTop
 $(function() {
-  $('a').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    $('a').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
 
-  // Portfolio
-  $('.box').hover(function () {
-    var height = $(this).height()/2;
-    // $(this).find('img').css('marginTop', '-'+height+'px');
-    $(this).find('.controllers').css('paddingTop', height+'px').slideToggle(500);
-  });
+    // Portfolio
+    $('.box').hover(function() {
+        var height = $(this).height() / 2;
+        // $(this).find('img').css('marginTop', '-'+height+'px');
+        $(this).find('.controllers').css('paddingTop', height + 'px').slideToggle(500);
+    });
 });
